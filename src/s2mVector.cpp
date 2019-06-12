512eee04700585a99c83d05339288e92aa3ae1a7
ValentinAllard <valentin.allard@rocketmail.com>
2019-06-12 16:30:52 -0400

    Added norm and normdot in s2mVector

--- src/s2mVector.cpp
+++ src/s2mVector.cpp
@@ -10,6 +10,31 @@ s2mVector::s2mVector(const s2mVector &v) : Eigen::VectorXd(v) {}
 
 s2mVector::s2mVector(unsigned int i) : Eigen::VectorXd(i) {}
 
+double s2mVector::norm(unsigned int p){
+    s2mError::s2mAssert(p>=2, "p must be superior or equal to 2");
+
+    double res(0);
+    for(unsigned int i=0; i < size(); ++i){
+        res += std::pow(fabs((*this)(i)), p);
+    }
+
+    return std::pow(res, 1.0/p);
+}
+
+double s2mVector::normdot(unsigned int p){
+    s2mError::s2mAssert(p>=2, "p must be superior or equal to 2");
+
+    double res1(0);
+    double res2(0);
+    for(unsigned int i=0; i < size(); ++i){
+        res1 += std::pow(p*fabs((*this)(i)), p-1);
+        res2 += std::pow(fabs((*this)(i)), p);
+    }
+    return res1*std::pow(res2, 1.0/p-1.0);
+}
+
+
+
 s2mVector::~s2mVector() {}
 
 Eigen::VectorXd s2mVector::vector() const{
