08c71ef59ba223faa7fc574b4ea0af862cc5adba
ValentinAllard <valentin.allard@rocketmail.com>
2019-08-02 19:19:08 -0400

    Added modification to setState so that total fiber quantity won't vary

--- src/s2mMuscleFatigueState.cpp
+++ src/s2mMuscleFatigueState.cpp
@@ -24,36 +24,47 @@ void s2mMuscleFatigueState::setState(double active, double fatigued, double rest
 {
     // Sanity check for active fibers
     if (active < 0){
+        resting += active;
         s2mError::s2mWarning(0, "Active Fibers Quantity can't be lower than 0, 0 is used then");
+        std::cout <<  "Previous Active Fibers Quantity before set to 0: " << active << std::endl;
         active = 0;
     }
     else if (active > 1){
+        resting += active - 1;
         s2mError::s2mWarning(0, "Active Fibers Quantity can't be higher than 1, 1 is used then");
+        std::cout <<  "Previous Active Fibers Quantity before set to 1: " << active << std::endl;
         active = 1;
     }
 
     // Sanity check for fatigued fibers
     if (fatigued < 0){
         s2mError::s2mWarning(0, "Fatigued Fibers Quantity can't be lower than 0, 0 is used then");
+        std::cout <<  "Previous Fatigued Fibers Quantity before set to 0: " << fatigued << std::endl;
         fatigued = 0;
     }
     else if (fatigued > 1){
         s2mError::s2mWarning(0, "Fatigued Fibers Quantity can't be higher than 1, 1 is used then");
+        std::cout <<  "Previous Fatigued Fibers Quantity before set to 1: " << fatigued << std::endl;
         fatigued = 1;
     }
 
     // Sanity check for resting fibers
     if (resting < 0){
+        active += resting;
         s2mError::s2mWarning(0, "Resting Fibers Quantity can't be lower than 0, 0 is used then");
+        std::cout <<  "Previous Resting Fibers Quantity before set to 0: " << resting << std::endl;
         resting = 0;
     }
     else if (resting > 1){
+        fatigued += 1 - resting;
         s2mError::s2mWarning(0, "Resting Fibers Quantity can't be higher than 1, 1 is used then");
+        std::cout <<  "Previous Resting Fibers Quantity before set to 1: " << resting << std::endl;
         resting = 1;
     }
 
-    if (active + fatigued + resting != 1.0)
+    if (fabs(active + fatigued + resting - 1.0) > 0.1){
         s2mError::s2mAssert(false, "Sum of the fatigued states must be equal to 1");
+    }
 
     m_activeFibers = active;
     m_fatiguedFibers = fatigued;
