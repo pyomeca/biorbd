b9be99de97cb25171db9b5843ea3930c6986ff49
Pariterre <pariterre@hotmail.com>
2019-08-20 23:45:58 +0200

    cleaned up a test

--- test/test_biorbd.cpp
+++ test/test_biorbd.cpp
@@ -39,15 +39,15 @@ TEST(Dynamics, Forward){
 }
 
 static std::string modelPathForLoopConstraintTesting("models/loopConstrainedModel.bioMod");
-TEST(Constraint, loopConstraint){
+TEST(Dynamics, ForwardLoopConstraint){
     biorbd::Model model(modelPathForLoopConstraintTesting);
     biorbd::rigidbody::GeneralizedCoordinates Q(model), QDot(model), QDDot_constrained(model), QDDot_expected(model);
     biorbd::rigidbody::GeneralizedTorque Tau(model);
     Q.setZero();
     QDot.setZero();
     QDDot_constrained.setZero();
-    QDDot_expected << 0.35935119225397999,  -10.110263945851218,  -6.0044250056047249e-14, -27.780105329642453, 31.783042765424835, 44.636461505611521,
-            0.67809701484785911, -6.251969045745929e-16, 5.9580819677934563e-15, 15.822263679783546, 2.4880587040480365e-15, -9.930136612989094e-15;
+    QDDot_expected << 0.35935119225397999,  -10.110263945851218,  0, -27.780105329642453, 31.783042765424835,
+            44.636461505611521, 0.67809701484785911, 0, 0, 15.822263679783546, 0, 0;
     Tau.setZero();
 
     biorbd::rigidbody::Contacts& cs(model.getConstraints_nonConst());
