6e027c5422c4ca7ac400e97ccf8e6a31a028a77b
Pariterre <pariterre@hotmail.com>
2019-08-27 12:23:32 +0200

    more tests for path and starting to integrate windows

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -8,47 +8,71 @@
 #include "Utils/RotoTransNode.h"
 
 TEST(Path, Create){
+    {
+        biorbd::utils::Path emptyPath;
+        EXPECT_STREQ(emptyPath.absolutePath().c_str(), biorbd::utils::Path::currentDir().c_str());
+        EXPECT_STREQ(emptyPath.filename().c_str(), "");
+        EXPECT_STREQ(emptyPath.extension().c_str(), "");
+        EXPECT_STREQ(emptyPath.originalPath().c_str(), "");
+    }
 
-    biorbd::utils::Path absolutePath("/MyLovely/AbsolutePath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(absolutePath.absolutePath().c_str(), "/MyLovely/AbsolutePath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(absolutePath.filename().c_str(), "ToMyLovelyFile");
-    EXPECT_STREQ(absolutePath.extension().c_str(), "biorbd");
-    EXPECT_STREQ(absolutePath.originalPath().c_str(), "/MyLovely/AbsolutePath/ToMyLovelyFile.biorbd");
+    {
+#ifdef _WIN32
+        biorbd::utils::String path("C:/MyLovely/AbsolutePath/ToMyLovelyFile.biorbd");
+#else
+        biorbd::utils::String path("/MyLovely/AbsolutePath/ToMyLovelyFile.biorbd");
+#endif
+        biorbd::utils::Path absolutePath(path);
+        EXPECT_STREQ(absolutePath.absolutePath().c_str(), "/MyLovely/AbsolutePath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(absolutePath.filename().c_str(), "ToMyLovelyFile");
+        EXPECT_STREQ(absolutePath.extension().c_str(), "biorbd");
+        EXPECT_STREQ(absolutePath.originalPath().c_str(), path.c_str());
+    }
 
-    biorbd::utils::Path relativePath("MyLovely/RelativePath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(relativePath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyLovely/RelativePath/ToMyLovelyFile.biorbd").c_str());
-    EXPECT_STREQ(relativePath.filename().c_str(), "ToMyLovelyFile");
-    EXPECT_STREQ(relativePath.extension().c_str(), "biorbd");
-    EXPECT_STREQ(relativePath.relativePath().c_str(), "./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(relativePath.originalPath().c_str(), "MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+    {
+        biorbd::utils::Path relativePath("MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(relativePath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyLovely/RelativePath/ToMyLovelyFile.biorbd").c_str());
+        EXPECT_STREQ(relativePath.filename().c_str(), "ToMyLovelyFile");
+        EXPECT_STREQ(relativePath.extension().c_str(), "biorbd");
+        EXPECT_STREQ(relativePath.relativePath().c_str(), "./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(relativePath.originalPath().c_str(), "MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+    }
 
-    biorbd::utils::Path weirdRelativePath("./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(weirdRelativePath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyLovely/RelativePath/ToMyLovelyFile.biorbd").c_str());
-    EXPECT_STREQ(weirdRelativePath.filename().c_str(), "ToMyLovelyFile");
-    EXPECT_STREQ(weirdRelativePath.extension().c_str(), "biorbd");
-    EXPECT_STREQ(weirdRelativePath.relativePath().c_str(), "./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(weirdRelativePath.originalPath().c_str(), "./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+    {
+        biorbd::utils::Path weirdRelativePath("./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(weirdRelativePath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyLovely/RelativePath/ToMyLovelyFile.biorbd").c_str());
+        EXPECT_STREQ(weirdRelativePath.filename().c_str(), "ToMyLovelyFile");
+        EXPECT_STREQ(weirdRelativePath.extension().c_str(), "biorbd");
+        EXPECT_STREQ(weirdRelativePath.relativePath().c_str(), "./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(weirdRelativePath.originalPath().c_str(), "./MyLovely/RelativePath/ToMyLovelyFile.biorbd");
+    }
 
-    biorbd::utils::Path parentRelativePath("../MyLovely/ParentPath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(parentRelativePath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "../MyLovely/ParentPath/ToMyLovelyFile.biorbd").c_str());
-    EXPECT_STREQ(parentRelativePath.filename().c_str(), "ToMyLovelyFile");
-    EXPECT_STREQ(parentRelativePath.extension().c_str(), "biorbd");
-    EXPECT_STREQ(parentRelativePath.relativePath().c_str(), "../MyLovely/ParentPath/ToMyLovelyFile.biorbd");
-    EXPECT_STREQ(parentRelativePath.originalPath().c_str(), "../MyLovely/ParentPath/ToMyLovelyFile.biorbd");
+    {
+        biorbd::utils::Path parentRelativePath("../MyLovely/ParentPath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(parentRelativePath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "../MyLovely/ParentPath/ToMyLovelyFile.biorbd").c_str());
+        EXPECT_STREQ(parentRelativePath.filename().c_str(), "ToMyLovelyFile");
+        EXPECT_STREQ(parentRelativePath.extension().c_str(), "biorbd");
+        EXPECT_STREQ(parentRelativePath.relativePath().c_str(), "../MyLovely/ParentPath/ToMyLovelyFile.biorbd");
+        EXPECT_STREQ(parentRelativePath.originalPath().c_str(), "../MyLovely/ParentPath/ToMyLovelyFile.biorbd");
+    }
 
-    biorbd::utils::Path noPath("MyLonelyFile.biorbd");
-    EXPECT_STREQ(noPath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyLonelyFile.biorbd").c_str());
-    EXPECT_STREQ(noPath.filename().c_str(), "MyLonelyFile");
-    EXPECT_STREQ(noPath.extension().c_str(), "biorbd");
-    EXPECT_STREQ(noPath.relativePath().c_str(), "./MyLonelyFile.biorbd");
-    EXPECT_STREQ(noPath.originalPath().c_str(), "MyLonelyFile.biorbd");
+    {
+        biorbd::utils::Path noPath("MyLonelyFile.biorbd");
+        EXPECT_STREQ(noPath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyLonelyFile.biorbd").c_str());
+        EXPECT_STREQ(noPath.filename().c_str(), "MyLonelyFile");
+        EXPECT_STREQ(noPath.extension().c_str(), "biorbd");
+        EXPECT_STREQ(noPath.relativePath().c_str(), "./MyLonelyFile.biorbd");
+        EXPECT_STREQ(noPath.originalPath().c_str(), "MyLonelyFile.biorbd");
+    }
 
-    biorbd::utils::Path almostNoPath("./MyKindOfLonelyFile.biorbd");
-    EXPECT_STREQ(almostNoPath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyKindOfLonelyFile.biorbd").c_str());
-    EXPECT_STREQ(almostNoPath.filename().c_str(), "MyKindOfLonelyFile");
-    EXPECT_STREQ(almostNoPath.extension().c_str(), "biorbd");
-    EXPECT_STREQ(almostNoPath.relativePath().c_str(), "./MyKindOfLonelyFile.biorbd");
-    EXPECT_STREQ(almostNoPath.originalPath().c_str(), "./MyKindOfLonelyFile.biorbd");
+    {
+        biorbd::utils::Path almostNoPath("./MyKindOfLonelyFile.biorbd");
+        EXPECT_STREQ(almostNoPath.absolutePath().c_str(), (biorbd::utils::Path::currentDir() + "MyKindOfLonelyFile.biorbd").c_str());
+        EXPECT_STREQ(almostNoPath.filename().c_str(), "MyKindOfLonelyFile");
+        EXPECT_STREQ(almostNoPath.extension().c_str(), "biorbd");
+        EXPECT_STREQ(almostNoPath.relativePath().c_str(), "./MyKindOfLonelyFile.biorbd");
+        EXPECT_STREQ(almostNoPath.originalPath().c_str(), "./MyKindOfLonelyFile.biorbd");
+    }
 }
 
 TEST(Path, Copy){
