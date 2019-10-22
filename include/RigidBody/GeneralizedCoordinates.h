badb39000b4101f11dc642b44fd0517b6d8c8439
Ariane <dang.ariane@gmail.com>
2019-10-22 11:54:57 -0400

    Added DOxygen comments to GeneralizedCoordinates.h
    Not sure if "Create is the right term"

--- include/RigidBody/GeneralizedCoordinates.h
+++ include/RigidBody/GeneralizedCoordinates.h
@@ -11,12 +11,37 @@ class Joints;
 class BIORBD_API GeneralizedCoordinates : public biorbd::utils::Vector
 {
 public:
+
+    ///
+    /// \brief Create generalized coordinates
+    /// 
     GeneralizedCoordinates();
+
+    ///
+    /// \brief Create generalized coordinates
+    /// \param Q State vector of the internal joints
+    ///
     GeneralizedCoordinates(const biorbd::rigidbody::GeneralizedCoordinates& Q);
+
+
     template<typename OtherDerived> GeneralizedCoordinates(const Eigen::MatrixBase<OtherDerived>& other) :
         biorbd::utils::Vector(other){}
+
+    ///
+    /// \brief Create generalized coordinates of a vector at a specified position
+    /// \param i Vector position
+    ///
     GeneralizedCoordinates(unsigned int i);
+
+    ///
+    /// \brief Create generalized coordinates of specified joint
+    /// \param j The joint
+    ///
     GeneralizedCoordinates(const biorbd::rigidbody::Joints& j);
+
+    ///
+    /// \brief Destroy the class properly
+    ///
     virtual ~GeneralizedCoordinates();
 
     template<typename OtherDerived>
