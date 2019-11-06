e05bc0f6c768b70963530149a6a2ff4a9d7039cb
Ariane <dang.ariane@gmail.com>
2019-11-06 09:37:12 -0500

    Doxygen comments in force.h

--- include/Muscles/Force.h
+++ include/Muscles/Force.h
@@ -34,34 +34,78 @@ public:
             double z);
 
     ///
-    /// \brief Construct force
-    /// \param x Position on X axis
-    /// \param y Position on Y axis
-    /// \param z Position on Z axis
-    /// 
+    /// \brief Construct force from another force
+    /// \param force The other force
+    ///
     Force(
             const biorbd::muscles::Force& force);
+
+    ///
+    /// \brief Construct force from another force
+    /// \param other The other force in vector form
+    ///
     template<typename OtherDerived> Force(const Eigen::MatrixBase<OtherDerived>& other) :
         Eigen::Vector3d(other){}
+
+    ///
+    /// \brief Construct force from another force
+    /// \param force Force in Node3D format
+    ///
     Force(
             const biorbd::utils::Node3d& force);
+
+    ///
+    /// \brief Construct force
+    /// \param geo The geometry of the muscle
+    /// \param vectorNorm The norm of the vector
+    ///
     Force(
             const biorbd::muscles::Geometry& geo,
             double vectorNorm);
+
+    ///
+    /// \brief Destroy class properly
+    ///
     virtual ~Force();
+
+    ///
+    /// \brief Deep copy of a force
+    /// \return A deep copy of the force
+    ///
     biorbd::muscles::Force DeepCopy() const;
+
+    ///
+    /// \brief Deep copy of a force
+    /// \param other The force to copy
+    ///
     void DeepCopy(const biorbd::muscles::Force& other);
 
-    // Get et set
+
+    ///
+    /// \brief Set the force from the muscle geometry
+    /// \param geo The muscle geometry
+    /// \param vectorNorm The norm of the vector
+    ///
     virtual void setForceFromMuscleGeometry(
             const biorbd::muscles::Geometry& geo,
             double vectorNorm);
 
+    ///
+    /// \brief Equal operator to be used with an Eigen matrix to construct force
+    /// \param other The eigen matrix of force
+    /// \return The force
+    ///
     template<typename OtherDerived>
         biorbd::muscles::Force& operator=(const Eigen::MatrixBase <OtherDerived>& other){
             this->Eigen::Vector3d::operator=(other);
             return *this;
         }
+
+    ///
+    /// \brief Equal operator to be used with another force
+    /// \param other Other force to copy
+    /// \return The new force
+    ///
     biorbd::muscles::Force& operator=(const biorbd::muscles::Force& other);
 };
 
