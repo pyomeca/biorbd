4d0b0d6d7972ddce60a3c883b92d9bcd703edf90
paul.wegiel <paul-wegiel@orange.fr>
2020-05-20 12:14:19 -0400

    Following dev of test_actuators.cpp

--- test/test_actuators.cpp
+++ test/test_actuators.cpp
@@ -30,6 +30,8 @@ TEST(FileIO, OpenModelActuators){
     EXPECT_NO_THROW(biorbd::Model model(modelPathForGeneralTesting));
 }
 
+//Tests for TorqueMax:
+
 TEST(Actuators, ActuatorConstant_TorqueMax){
     biorbd::actuator::ActuatorConstant const_torque_act(1, 150, 0);
     EXPECT_NEAR(const_torque_act.torqueMax(), 150, requiredPrecision);
@@ -76,4 +78,87 @@ TEST(Actuators, ActuatorLinear_TorqueMax){
     biorbd::actuator::ActuatorLinear linear_torque_act(1, 25, 1, 0);
     // Whatever the value of the slope, linear_torque_act.torqueMax returns 25, it seems normal to me as it's linear, though (?)
     EXPECT_NEAR(linear_torque_act.torqueMax(Q), torque_max_expected, requiredPrecision);
+}
+
+//Tests for torque method:
+
+TEST(Actuators, ActuatorConstant_Torque){
+
+    biorbd::Model model(modelPathForGeneralTesting);
+    DECLARE_GENERALIZED_COORDINATES(Q, model);
+    DECLARE_GENERALIZED_VELOCITY(QDot, model);
+
+    // Set to random values
+    std::vector<double> val(model.nbQ());
+    for (size_t i=0; i<val.size(); i++){
+        val[i] = static_cast<double>(i) * 1.1;
+    }
+    FILL_VECTOR(Q, val);
+    FILL_VECTOR(QDot, val);
+    biorbd::actuator::ActuatorConstant const_torque_act(1, 150, 0);
+    //biorbd::utils::Vector vector_torque_act(1);
+    biorbd::rigidbody::GeneralizedTorque Tau(model);
+    //Tau = model.torque(vector_torque_act, Q, QDot);
+    Tau = model.torque(const_torque_act, Q, QDot);
+    EXPECT_NEAR(Tau, 150, requiredPrecision);
+}
+
+TEST(Actuators, ActuatorGauss3p_Torque){
+
+    biorbd::Model model(modelPathForGeneralTesting);
+    DECLARE_GENERALIZED_COORDINATES(Q, model);
+    DECLARE_GENERALIZED_VELOCITY(QDot, model);
+
+    // Set to random values
+    std::vector<double> val(model.nbQ());
+    for (size_t i=0; i<val.size(); i++){
+        val[i] = static_cast<double>(i) * 1.1;
+    }
+    FILL_VECTOR(Q, val);
+    FILL_VECTOR(QDot, val);
+    biorbd::actuator::ActuatorGauss3p gauss3p_torque_act(1, 150, 25, 800, 324, 0.5, 28, 90, 29, 133, 0);
+    //biorbd::utils::Vector vector_torque_act(1);
+    biorbd::rigidbody::GeneralizedTorque Tau(model);
+    Tau = model.torque(gauss3p_torque_act, Q, QDot);
+    EXPECT_NEAR(Tau, 150, requiredPrecision);
+}
+
+TEST(Actuators, ActuatorGauss6p_Torque){
+
+    biorbd::Model model(modelPathForGeneralTesting);
+    DECLARE_GENERALIZED_COORDINATES(Q, model);
+    DECLARE_GENERALIZED_VELOCITY(QDot, model);
+
+    // Set to random values
+    std::vector<double> val(model.nbQ());
+    for (size_t i=0; i<val.size(); i++){
+        val[i] = static_cast<double>(i) * 1.1;
+    }
+    FILL_VECTOR(Q, val);
+    FILL_VECTOR(QDot, val);
+    biorbd::actuator::ActuatorGauss6p gauss6p_torque_act(1, 150, 25, 800, 324, 0.5, 28, 90, 29, 133, 4, 73, 73, 0);
+    //biorbd::utils::Vector vector_torque_act(1);
+    biorbd::rigidbody::GeneralizedTorque Tau(model);
+    Tau = model.torque(gauss6p_torque_act, Q, QDot);
+    EXPECT_NEAR(Tau, 150, requiredPrecision);
+}
+
+TEST(Actuators, ActuatorLinear_Torque){
+
+    biorbd::Model model(modelPathForGeneralTesting);
+    DECLARE_GENERALIZED_COORDINATES(Q, model);
+    DECLARE_GENERALIZED_VELOCITY(QDot, model);
+
+    // Set to random values
+    std::vector<double> val(model.nbQ());
+    for (size_t i=0; i<val.size(); i++){
+        val[i] = static_cast<double>(i) * 1.1;
+    }
+    FILL_VECTOR(Q, val);
+    FILL_VECTOR(QDot, val);
+    biorbd::actuator::ActuatorLinear linear_torque_act(1, 25, 1, 0);
+    //biorbd::utils::Vector vector_torque_act(1);
+    biorbd::rigidbody::GeneralizedTorque Tau(model);
+    Tau = model.torque(linear_torque_act, Q, QDot);
+    EXPECT_NEAR(Tau, 150, requiredPrecision);
 }
