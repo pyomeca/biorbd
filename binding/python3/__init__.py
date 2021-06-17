b4c8df35afb46590c41146d250c59774ee9d8693
Pariterre <pariterre@hotmail.com>
2021-06-17 16:41:30 +0200

    Allowed to_casadi to be directly MX values

--- binding/python3/__init__.py
+++ binding/python3/__init__.py
@@ -4,17 +4,21 @@ from .surface_max_torque_actuator import *
 from .rigid_body import *
 
 if biorbd.currentLinearAlgebraBackend() == 1:
-    from casadi import Function, MX, horzcat
+    from casadi import Function, MX, SX, horzcat
 
     def to_casadi_func(name, func, *all_param):
-        mx_param = []
+        cx_param = []
         for p in all_param:
-            if isinstance(p, MX):
-                mx_param.append(p)
+            if isinstance(p, (MX, SX)):
+                cx_param.append(p)
 
-        func_evaluated = func(*all_param)
-        if isinstance(func_evaluated, (list, tuple)):
-            func_evaluated = horzcat(*[val if isinstance(val, MX) else val.to_mx() for val in func_evaluated])
-        elif not isinstance(func_evaluated, MX):
-            func_evaluated = func_evaluated.to_mx()
-        return Function(name, mx_param, [func_evaluated]).expand()
+        if callable(func):
+            func_evaluated = func(*all_param)
+            if isinstance(func_evaluated, (list, tuple)):
+                func_evaluated = horzcat(*[val if isinstance(val, MX) else val.to_mx() for val in func_evaluated])
+            elif not isinstance(func_evaluated, MX):
+                func_evaluated = func_evaluated.to_mx()
+        else:
+            func_evaluated = func
+        return Function(name, cx_param, [func_evaluated]).expand()
+
