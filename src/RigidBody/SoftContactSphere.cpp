fee6076b5b2593dfdc8538faacd707d36cf58347
Ipuch <puchaud.pierre@gmail.com>
2021-09-27 18:31:53 -0400

    1st guess on writting contact force computation

--- src/RigidBody/SoftContactSphere.cpp
+++ src/RigidBody/SoftContactSphere.cpp
@@ -142,12 +142,64 @@ RigidBodyDynamics::Math::SpatialVector rigidbody::SoftContactSphere::computeForc
         const rigidbody::GeneralizedCoordinates& Q,
         const rigidbody::GeneralizedVelocity& QDot) const
 {
-    RigidBodyDynamics::Math::SpatialVector out(0, 0, 0, 0, 0, 0);
+    RigidBodyDynamics::Math::SpatialVector f(0, 0, 0, 0, 0, 0);
 
-    // out[0] = *m_damping + *m_stiffness * QDot.cross(QDot).dot(QDot) * QDot(0);
-    // FILL THIS WITH ACTUAL COMPUTATION
+    // Contact Position and velocity
+    x = rigidbody::SoftContacts::softContact(Q)
+    dx = rigidbody::SoftContacts::softContactVelocity(Q, QDot)
 
-    return out;
+    // Indentation detection with the ground
+    RigidBodyDynamics::Math::Vector3d grd(0,0,0);
+    RigidBodyDynamics::Math::Vector3d n_vec(0,0,1); // normal
+
+    // Decomposition des vitesses
+    vn = dx.dot(n_vec)
+    vt = dx - vn * n_vec
+
+    // Indentation
+    delta = - ((x - grd).dot(n_vec) - rigidbody::SoftContactSphere::radius())
+    deltadot = vn
+
+    // location of the normal force
+    p = (x - grd) - delta * n_vec
+
+    // Smoothed contact
+    eps = 1e-16
+    bv = 50
+    bd = 300
+    fslope = (1 / 2 + 1 / 2 * tanh(bd * delta) + eps) * (
+                1 / 2 + 1 / 2 * tanh(bv * (deltadot + 2 / 3 / rigidbody::SoftContacts::SoftContactSphere::damping() ) + eps))
+
+    // Normal force
+    // Hertz
+    fHz = 4 / 3 * rigidbody::SoftContacts::SoftContactSphere::stiffness() * sqrt(rigidbody::SoftContacts::SoftContactSphere::radius()) * sqrt(delta ** 2) ** (3 / 2)  # // sqrt and **2 to get positive values in case of negative indentation
+    // Hunt-Crossley
+    fHC = fHz * (1 + 3 / 2 * rigidbody::SoftContacts::SoftContactSphere::damping() * deltadot)
+    fn = fHc * fslope
+
+    // Friction
+    mu_s = 0.8
+    mu_d = 0.7
+    mu_v = 0.5
+    vtrans = 0.1 // from 0.01 to 0.13
+
+    vt_norm = sqrt((vt[0]**2+vt[1]**2+vt[2]**2 + 1e-5))
+    vrel = vt_norm / vtrans
+
+    f_fric = fn * mu_d * tanh(4 * vrel) + fn * (mu_s - mu_d) * vrel / (1 / 4 * vrel ** 2 + 3 / 4) ** 2 + fn * mu_v * vt_norm  // from Peter Brown 2017
+
+    // Total Force
+    f(3:5) = fn * n_vec + f_fric * -vt / vt_norm
+
+    // Location of the center of mass
+    // parent segment idx
+    // CoM = center of mass location
+
+    // Bour's formula to the CoM of the segment
+    fCoM = f // force
+    fp[0:3] = f[0:3] + cross(f[3:], CoM - p) // torques
+
+    return f;
 }
 
 void rigidbody::SoftContactSphere::setType()
