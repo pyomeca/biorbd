11c28a5d3b25496708f54bbdd8a32f9d4b8dc4c7
AurelienRenou <aurelien.renou@ens2m.org>
2022-06-06 13:57:16 -0400

    test blacked

--- test/binding/Python3/test_inverse_kinematics.py
+++ test/binding/Python3/test_inverse_kinematics.py
@@ -24,14 +24,10 @@ def test_solve(brbd, method):
     ik = biorbd.InverseKinematics(biorbd_model, markers)
     ik_q = ik.solve(method=method)
 
-    if method =="only_lm":
+    if method == "only_lm":
         np.testing.assert_almost_equal(
             np.squeeze(ik_q.T),
             qinit,
         )
     elif method == "trf" or method == "lm":
-        np.testing.assert_almost_equal(
-            np.squeeze(np.round(ik_q, 1).T),
-            qinit, decimal=1
-        )
-
+        np.testing.assert_almost_equal(np.squeeze(np.round(ik_q, 1).T), qinit, decimal=1)
