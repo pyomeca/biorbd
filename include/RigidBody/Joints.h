b9ed314b47ae74c203b3abc85d7d40320352b5c1
Ipuch <puchaud.pierre@gmail.com>
2022-07-14 11:16:02 -0400

    docstring

--- include/RigidBody/Joints.h
+++ include/RigidBody/Joints.h
@@ -891,6 +891,7 @@ public:
     /// \param QDot The Generalized Velocities
     /// \param Tau The Generalized Torques
     /// \param f_ext External force acting on the system if there are any
+    /// \param f_contacts The forces applied to the rigid contacts if there are any
     /// \return The Generalized Accelerations
     ///
     rigidbody::GeneralizedAcceleration ForwardDynamics(
