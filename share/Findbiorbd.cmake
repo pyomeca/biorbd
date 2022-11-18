a4178563495f4313bd311ccbf6154af95cda231a
Amedeo <amedeo.ceglia@umontreal.ca>
2022-11-18 12:09:56 -0500

    Fix path for cpp_example

--- share/Findbiorbd.cmake
+++ share/Findbiorbd.cmake
@@ -29,7 +29,7 @@ set(biorbd_LIBRARIES
     ${biorbd_LIB_PATH}/${biorbd_LIB_NAME}_internal_forces${debug_flag}${biorbd_LIB_EXTENSION}
     ${biorbd_LIB_PATH}/${biorbd_LIB_NAME}_muscles${debug_flag}${biorbd_LIB_EXTENSION}
     ${biorbd_LIB_PATH}/${biorbd_LIB_NAME}_actuators${debug_flag}${biorbd_LIB_EXTENSION}
-        ${biorbd_LIB_PATH}/${biorbd_LIB_NAME}_passive_torques${debug_flag}${biorbd_LIB_EXTENSION}
+    ${biorbd_LIB_PATH}/${biorbd_LIB_NAME}_passive_torques${debug_flag}${biorbd_LIB_EXTENSION}
 )
 
 # handle the QUIETLY and REQUIRED arguments and set biorbd_FOUND to TRUE if 
