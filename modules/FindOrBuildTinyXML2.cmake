35e1c9f80bb704fdd73d58fba5637fc3985428fc
Pariterre <pariterre@hotmail.com>
2025-03-13 15:25:12 -0400

    Moved to tinyxml2

---
+++ modules/FindOrBuildTinyXML2.cmake
@@ -0,0 +1,35 @@
+macro(FindOrBuildTinyXML2)   
+    message(STATUS "Downloading TinyXML2 version 10.1.0 and installing from GitHub")
+    include(ExternalProject)
+
+    set(TinyXML2_IS_BUILT TRUE)
+    set(TinyXML2_INSTALL_DIR "${CMAKE_BINARY_DIR}/TinyXML2_install")
+
+    # Detect correct static library extension (OS-independent)
+    if(WIN32)
+        set(TinyXML2_LIB_NAME "tinyxml2.lib")
+    else()
+        set(TinyXML2_LIB_NAME "libtinyxml2.a")
+    endif()
+
+    ExternalProject_Add(TinyXML2_external
+        GIT_REPOSITORY https://github.com/leethomason/tinyxml2.git
+        GIT_TAG 10.1.0
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${TinyXML2_INSTALL_DIR}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DBUILD_SHARED_LIBS=OFF
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        BUILD_BYPRODUCTS "${TinyXML2_INSTALL_DIR}/lib/${TinyXML2_LIB_NAME}"
+    )
+
+    # Define outputs
+    set(TinyXML2_DIR "${TinyXML2_INSTALL_DIR}/share/tinyxml2/cmake")
+    set(TinyXML2_INCLUDE_DIR "${TinyXML2_INSTALL_DIR}/include")
+    set(TinyXML2_LIBRARY "${TinyXML2_INSTALL_DIR}/lib/${TinyXML2_LIB_NAME}")
+    set(TinyXML2_FOUND TRUE INTERNAL "TinyXML2 found or built")
+
+    # Ensure all targets depending on TinyXML2 wait for it to build
+    add_library(TinyXML2 INTERFACE IMPORTED)
+    add_dependencies(TinyXML2 TinyXML2_external)
+endmacro()
