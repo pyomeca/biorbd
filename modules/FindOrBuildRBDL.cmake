7506fed8610ae0a9e605d020bdfbba28e27169a1
Pariterre <pariterre@hotmail.com>
2025-08-25 17:56:32 -0400

    Wrap RBDL to use modern CMake

--- modules/FindOrBuildRBDL.cmake
+++ modules/FindOrBuildRBDL.cmake
@@ -1,141 +1,166 @@
-macro(FindOrBuildRBDL MATH_BACKEND)
-    # Check if MATH_BACKEND is EIGEN or CASADI
+function(FindOrBuildRBDL MATH_BACKEND)
+    include(ExternalProject)
+
+    # -------------------------------
+    # Validate backend
+    # -------------------------------
     if(NOT (${MATH_BACKEND} STREQUAL "EIGEN" OR ${MATH_BACKEND} STREQUAL "CASADI"))
         message(FATAL_ERROR "FindOrBuildRBDL: Invalid option '${MATH_BACKEND}'. Use 'EIGEN' or 'CASADI'.")
     endif()
 
-    # Try to find RBDL first
-    if (${MATH_BACKEND} STREQUAL "EIGEN")
-        set (CUSTOM_RBDL_PATH ${INSTALL_DEPENDENCIES_PREFIX})
+    # -------------------------------
+    # Try finding existing install
+    # -------------------------------
+    if(${MATH_BACKEND} STREQUAL "EIGEN")
         find_package(RBDL QUIET
-            PATHS ${CUSTOM_RBDLCasadi_PATH} ${CMAKE_INSTALL_PREFIX}
+            PATHS ${CUSTOM_RBDL_PATH} ${INSTALL_DEPENDENCIES_PREFIX} ${CMAKE_INSTALL_PREFIX}
         )
-    
-    elseif (${MATH_BACKEND} STREQUAL "CASADI")
-        set(RBDL_FOUND FALSE)
-        set (CUSTOM_RBDLCasadi_PATH ${INSTALL_DEPENDENCIES_PREFIX})
-        find_package(RBDLCasadi QUIET
-            PATHS ${CUSTOM_RBDLCasadi_PATH} ${CMAKE_INSTALL_PREFIX}
-        )
+        if(TARGET RBDL::RBDL)
+            message(STATUS "Found RBDL (Eigen backend) with modern target")
+            return()
+        elseif(RBDL_FOUND)
+            message(STATUS "Found RBDL (Eigen backend), wrapping legacy variables")
+            add_library(RBDL::RBDL UNKNOWN IMPORTED)
+            set_target_properties(RBDL::RBDL PROPERTIES
+                IMPORTED_LOCATION "${RBDL_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${RBDL_INCLUDE_DIR}"
+            )
+            return()
+        endif()
 
-        if (RBDLCasadi_FOUND)
-            set(RBDL_FOUND TRUE)
-            
-            # Define include and library paths to mimic RBDL eigen which is the format expected by biorbd
-            set(RBDL_INCLUDE_DIR ${RBDLCasadi_INCLUDE_DIR})
-            set(RBDL_LIBRARY ${RBDLCasadi_LIBRARY})
+    elseif(MATH_BACKEND STREQUAL "CASADI")
+        find_package(RBDLCasadi QUIET
+            PATHS ${CUSTOM_RBDLCasadi_PATH} ${INSTALL_DEPENDENCIES_PREFIX} ${CMAKE_INSTALL_PREFIX}
+        )
+        if(TARGET RBDL::RBDL)
+            message(STATUS "Found RBDL (CasADi backend) with modern target")
+            return()
+        elseif(RBDLCasadi_FOUND)
+            message(STATUS "Found RBDL (CasADi backend), wrapping legacy variables")
+            add_library(RBDL::RBDL UNKNOWN IMPORTED)
+            set_target_properties(RBDL::RBDL PROPERTIES
+                IMPORTED_LOCATION "${RBDLCasadi_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${RBDLCasadi_INCLUDE_DIR}"
+            )
+            return()
         endif()
     endif()
 
-    if(RBDL_FOUND AND NOT RBDL_IS_BUILT)
-        message (STATUS "RBDL found")
-        set(RBDL_IS_BUILT FALSE)
+    # -------------------------------
+    # If not found → build from source
+    # -------------------------------
+    message(STATUS "RBDL not found, building from GitHub")
+
+    if(NOT INSTALL_DEPENDENCIES_PREFIX OR INSTALL_DEPENDENCIES_PREFIX STREQUAL "")
+        set(RBDL_INSTALL_DIR "${CMAKE_BINARY_DIR}/RBDL_install")
+    else()
+        set(RBDL_INSTALL_DIR ${INSTALL_DEPENDENCIES_PREFIX})
+    endif()
+
+    # Backend config
+    if(${MATH_BACKEND} STREQUAL "EIGEN")
+        set(RBDL_BUILD_CASADI OFF)
+        set(RBDL_LIBRARY_SUFFIX "")
+        set(Casadi_DIR "")
+    elseif (${MATH_BACKEND} STREQUAL "CASADI")
+        set(RBDL_BUILD_CASADI ON)
+        set(RBDL_LIBRARY_SUFFIX "-casadi")
+
+        # If CasADi is already found, use it
+        if(${Casadi_FOUND})
+            set(Casadi_DIR "${Casadi_DIR}")
+        else()
+            # Else assume superbuild is providing it → use its install dir
+            set(Casadi_DIR "${Casadi_INSTALL_DIR}")
+        endif()
 
     else()
-        message(STATUS "RBDL not found, using version from GitHub")
-        set(RBDL_IS_BUILT TRUE)
-        include(ExternalProject)
-
-        if (NOT INSTALL_DEPENDENCIES_PREFIX OR INSTALL_DEPENDENCIES_PREFIX STREQUAL "")
-            set(RBDL_INSTALL_DIR "${CMAKE_BINARY_DIR}/RBDL_install")
-        else()
-            set(RBDL_INSTALL_DIR ${INSTALL_DEPENDENCIES_PREFIX})
-        endif()
-
-        if (${MATH_BACKEND} STREQUAL "EIGEN")
-            set(RBDL_BUILD_CASADI OFF)
-            set(RBDL_LIBRARY_SUFFIX "")
-            set(Casadi_DIR "")
-        elseif (${MATH_BACKEND} STREQUAL "CASADI")
-            set(RBDL_BUILD_CASADI ON)
-            set(RBDL_LIBRARY_SUFFIX "-casadi")
-            if (Casadi_IS_BUILT)
-                set(Casadi_DIR "${Casadi_INSTALL_DIR}")
-            else()
-                if (NOT Casadi_FOUND)
-                    set(Casadi_DIR "${CMAKE_INSTALL_PREFIX}")
-                    unset(Casadi_INCLUDE_DIR)
-                    unset(Casadi_LIBRARY)
-                endif()
-            endif()
-        endif()
-        
-        # Detect correct static library extension (OS-independent)
-        if (BUILD_SHARED_LIBS)
-            set(RBDL_TARGET_STATIC OFF)
-        else()
-            set(RBDL_TARGET_STATIC ON)
-        endif()
-        if(WIN32)
-            set(RBDL_LIB_NAME "rbdl${RBDL_LIBRARY_SUFFIX}.lib") 
-        else()
-            if(${RBDL_TARGET_STATIC})
-                set(RBDL_LIB_NAME "librbdl${RBDL_LIBRARY_SUFFIX}.a")
-            else()
-                if(LINUX)
-                    set(RBDL_LIB_NAME "librbdl${RBDL_LIBRARY_SUFFIX}.so")
-                elseif(APPLE)
-                    set(RBDL_LIB_NAME "librbdl${RBDL_LIBRARY_SUFFIX}.dylib")
-                else()
-                    message(FATAL_ERROR "Unsupported OS")
-                endif() 
-            endif()
-        endif()
-
-        set(RBDL_LIBRARY "${RBDL_INSTALL_DIR}/lib/${RBDL_LIB_NAME}")
-        ExternalProject_Add(rbdl_external
-            GIT_REPOSITORY https://github.com/pariterre/rbdl.git
-            GIT_TAG master
-            CMAKE_ARGS
-                -DCMAKE_INSTALL_PREFIX=${RBDL_INSTALL_DIR}
-                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-                -DRBDL_BUILD_STATIC=${RBDL_TARGET_STATIC}
-                -DRBDL_BUILD_CASADI=${RBDL_BUILD_CASADI}
-                -DEigen3_DIR=${EIGEN3_DIR}
-                -DCasadi_DIR=${Casadi_DIR}
-                -DCasadi_INCLUDE_DIR=${Casadi_INCLUDE_DIR}
-                -DCasadi_LIBRARY=${Casadi_LIBRARY}
-                -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-            BUILD_BYPRODUCTS "${RBDL_LIBRARY}"
-        )
-
-        # Define include and library paths
-        if (${MATH_BACKEND} STREQUAL "CASADI")
-            # Append "rbdl-casadi" to the include path
-            set(RBDL_INCLUDE_DIR "${RBDL_INSTALL_DIR}/include/rbdl-casadi")
-        else()
-            set(RBDL_INCLUDE_DIR "${RBDL_INSTALL_DIR}/include")
-        endif()
-
-        # Ensure that the library gets built before linking
-        add_library(RBDL_BUILD STATIC IMPORTED)
-        
-        set_target_properties(RBDL_BUILD PROPERTIES
-            IMPORTED_LOCATION "${RBDL_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${RBDL_INCLUDE_DIR}"
-        )
-        add_dependencies(RBDL_BUILD rbdl_external)
-
-        # Windows DLL runtime setup
-        if(WIN32)
-            set_target_properties(RBDL_BUILD PROPERTIES
-                IMPORTED_IMPLIB "${RBDL_LIBRARY}"  # Import library for linking
-                IMPORTED_LOCATION "${RBDL_RUNTIME_DIR}/rbdl.dll"  # Runtime DLL location
-            )
-        endif()
-
-        if (EIGEN3_IS_BUILT)
-            add_dependencies(rbdl_external EIGEN3_BUILD)
-        endif()
-        if (${MATH_BACKEND} STREQUAL "CASADI")
-            if (Casadi_IS_BUILT)
-                add_dependencies(rbdl_external CASADI_BUILD)
-            endif()
-            target_include_directories(RBDL_BUILD INTERFACE ${Casadi_INCLUDE_DIR})
-            target_link_libraries(RBDL_BUILD INTERFACE ${Casadi_LIBRARY})
-        endif()
-
-        set(RBDL_FOUND TRUE)
+		message(FATAL_ERROR "Backend not implemented")
     endif()
 
-endmacro()
+    # Detect correct static library extension (OS-independent)
+    if(BUILD_SHARED_LIBS)
+        set(RBDL_TARGET_STATIC OFF)
+    else()
+        set(RBDL_TARGET_STATIC ON)
+    endif()
+
+    if(WIN32)
+        set(RBDL_LIB_NAME "rbdl${RBDL_LIBRARY_SUFFIX}.lib") 
+    else()
+        if(${RBDL_TARGET_STATIC})
+            set(RBDL_LIB_NAME "librbdl${RBDL_LIBRARY_SUFFIX}.a")
+        else()
+            if(APPLE)
+                set(RBDL_LIB_NAME "librbdl${RBDL_LIBRARY_SUFFIX}.dylib")
+            elseif(UNIX)
+                set(RBDL_LIB_NAME "librbdl${RBDL_LIBRARY_SUFFIX}.so")
+            else()
+                message(FATAL_ERROR "Unsupported OS")
+            endif()
+        endif()
+    endif()
+
+    set(RBDL_LIBRARY "${RBDL_INSTALL_DIR}/lib/${RBDL_LIB_NAME}")
+    set(RBDL_DEPENDS "")
+    if(MATH_BACKEND STREQUAL "CASADI" AND NOT Casadi_FOUND)
+        list(APPEND RBDL_DEPENDS casadi_external)
+    endif()
+
+    ExternalProject_Add(rbdl_external
+        GIT_REPOSITORY https://github.com/pariterre/rbdl.git
+        GIT_TAG master
+        DEPENDS  ${RBDL_DEPENDS}
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${RBDL_INSTALL_DIR}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DRBDL_BUILD_STATIC=${RBDL_TARGET_STATIC}
+            -DRBDL_BUILD_CASADI=${RBDL_BUILD_CASADI}
+            -DEigen3_DIR=${EIGEN3_DIR}
+            -DCasadi_DIR=${Casadi_DIR}
+            -DCasadi_INCLUDE_DIR=${Casadi_INCLUDE_DIR}
+            -DCasadi_LIBRARY=${Casadi_LIBRARY}
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        BUILD_BYPRODUCTS "${RBDL_LIBRARY}"
+    )
+
+    # Define include and library paths
+    if(${MATH_BACKEND} STREQUAL "CASADI")
+        set(RBDL_INCLUDE_DIR "${RBDL_INSTALL_DIR}/include/rbdl-casadi")
+    else()
+        set(RBDL_INCLUDE_DIR "${RBDL_INSTALL_DIR}/include")
+    endif()
+
+    # Define imported target
+    add_library(RBDL::RBDL UNKNOWN IMPORTED)
+    set_target_properties(RBDL::RBDL PROPERTIES
+        IMPORTED_LOCATION "${RBDL_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${RBDL_INCLUDE_DIR}"
+    )
+    add_dependencies(RBDL::RBDL rbdl_external)
+    
+    # Ensure directory exists at configure time
+    file(MAKE_DIRECTORY "${RBDL_INCLUDE_DIR}")
+    
+    # Windows DLL import handling
+    if(WIN32)
+        set_target_properties(RBDL::RBDL PROPERTIES
+            IMPORTED_IMPLIB "${RBDL_LIBRARY}"
+            IMPORTED_LOCATION "${RBDL_RUNTIME_DIR}/rbdl.dll"
+        )
+    endif()
+
+    # Dependencies
+    if(${EIGEN3_IS_BUILT})
+        add_dependencies(rbdl_external EIGEN3_BUILD)
+    endif()
+    if(${MATH_BACKEND} STREQUAL "CASADI")
+        if(Casadi_FOUND)
+            target_link_libraries(RBDL::RBDL INTERFACE ${Casadi_LIBRARY})
+            target_include_directories(RBDL::RBDL INTERFACE ${Casadi_INCLUDE_DIR})
+        else()
+            add_dependencies(rbdl_external casadi_external)
+            target_link_libraries(RBDL::RBDL INTERFACE Casadi::Casadi)
+        endif()
+    endif()
+endfunction()
