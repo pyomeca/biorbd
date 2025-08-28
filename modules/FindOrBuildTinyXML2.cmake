f50c1ddd35f516d944c565eb7311864986a54b94
Pariterre <pariterre@hotmail.com>
2025-08-28 15:19:43 -0400

    Also did tinyxml2

--- modules/FindOrBuildTinyXML2.cmake
+++ modules/FindOrBuildTinyXML2.cmake
@@ -1,78 +1,61 @@
-macro(FindOrBuildTinyXML2)  
+function(FindOrBuildTinyXML2)
+    # -------------------------------
+    # Try finding existing install
+    # -------------------------------
     find_package(TinyXML2 QUIET
-        PATHS ${INSTALL_DEPENDENCIES_PREFIX}
+        PATHS ${INSTALL_DEPENDENCIES_PREFIX} ${CMAKE_INSTALL_PREFIX}
     )
 
-    if (TinyXML2_FOUND AND NOT TinyXML2e_IS_BUILT)
+    if (TARGET tinyxml2::tinyxml2)
         message (STATUS "TinyXML2 found")
-        set(TinyXML2_IS_BUILT FALSE)
-
-        if (NOT DEFINED TinyXML2_INCLUDE_DIR OR TinyXML2_INCLUDE_DIR STREQUAL "")
-            # Modern CMake does not set INCLUDE_DIR, so make it retro-compatible
-            get_target_property(TinyXML2_INCLUDE_DIR tinyxml2::tinyxml2 INTERFACE_INCLUDE_DIRECTORIES)
-            
-            # We once tried to get it from INTERFACE_LINK_LIBRARIES but it was not working
-            if(WIN32)
-                file(GLOB_RECURSE TinyXML2_LIBRARY "${TinyXML2_INCLUDE_DIR}/../tinyxml2.lib")
-            elseif(LINUX)
-                file(GLOB_RECURSE TinyXML2_LIBRARY "${TinyXML2_INCLUDE_DIR}/../libtinyxml2.so")
-                if (NOT TinyXML2_LIBRARY)
-                    file(GLOB_RECURSE TinyXML2_LIBRARY "${TinyXML2_INCLUDE_DIR}/../libtinyxml2.a")
-                endif()
-            elseif(APPLE)
-                file(GLOB_RECURSE TinyXML2_LIBRARY "${TinyXML2_INCLUDE_DIR}/../libtinyxml2.dylib")
-                if (NOT TinyXML2_LIBRARY)
-                    file(GLOB_RECURSE TinyXML2_LIBRARY "${TinyXML2_INCLUDE_DIR}/../libtinyxml2.a")
-                endif()
-            else()
-                message(FATAL_ERROR "Unsupported OS")
-            endif()
-        endif()
-
-    else()
-        message(STATUS "TinyXML2 not found, using version 10.1.0 from GitHub")
-        set(TinyXML2_IS_BUILT TRUE)
-        include(ExternalProject)
-
-        if (NOT INSTALL_DEPENDENCIES_PREFIX OR INSTALL_DEPENDENCIES_PREFIX STREQUAL "")
-            set(TinyXML2_INSTALL_DIR "${CMAKE_BINARY_DIR}/TinyXML2_install")
-        else()
-            set(TinyXML2_INSTALL_DIR ${INSTALL_DEPENDENCIES_PREFIX})
-        endif()
-
-        # Detect correct static library extension (OS-independent)
-        set(TynyXML2_BUILD_SHARED_LIBS OFF)
-        if(WIN32)
-            set(TinyXML2_LIB_NAME "tinyxml2.lib")
-        elseif(LINUX)
-            set(TinyXML2_LIB_NAME "libtinyxml2.a")
-        elseif(APPLE)
-            set(TinyXML2_LIB_NAME "libtinyxml2.a")
-        else()
-            message(FATAL_ERROR "Unsupported OS")
-        endif()
-
-        ExternalProject_Add(TinyXML2_external
-            GIT_REPOSITORY https://github.com/leethomason/tinyxml2.git
-            GIT_TAG 10.1.0
-            CMAKE_ARGS
-                -DCMAKE_INSTALL_PREFIX=${TinyXML2_INSTALL_DIR}
-                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-                -DBUILD_SHARED_LIBS=${TynyXML2_BUILD_SHARED_LIBS}
-                -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-            BUILD_BYPRODUCTS "${TinyXML2_INSTALL_DIR}/lib/${TinyXML2_LIB_NAME}"
-        )
-
-        # Define outputs
-        set(TinyXML2_DIR "${TinyXML2_INSTALL_DIR}/share/tinyxml2/cmake")
-        set(TinyXML2_INCLUDE_DIR "${TinyXML2_INSTALL_DIR}/include")
-        set(TinyXML2_LIBRARY "${TinyXML2_INSTALL_DIR}/lib/${TinyXML2_LIB_NAME}")
-        set(TinyXML2_FOUND TRUE)
-
-        # Ensure all targets depending on TinyXML2 wait for it to build
-        add_library(TinyXML2_BUILD INTERFACE IMPORTED)
-        add_dependencies(TinyXML2_BUILD TinyXML2_external)
-
+        return()
     endif()
 
-endmacro()
+    # -------------------------------
+    # If not found → build from source
+    # -------------------------------
+    message(STATUS "TinyXML2 not found, using version 10.1.0 from GitHub")
+    include(ExternalProject)
+
+    if (NOT INSTALL_DEPENDENCIES_PREFIX OR INSTALL_DEPENDENCIES_PREFIX STREQUAL "")
+        set(TinyXML2_INSTALL_DIR "${CMAKE_BINARY_DIR}/TinyXML2_install")
+    else()
+        set(TinyXML2_INSTALL_DIR ${INSTALL_DEPENDENCIES_PREFIX})
+    endif()
+
+    # Detect correct static library extension (OS-independent)
+    set(TynyXML2_BUILD_SHARED_LIBS OFF)
+    if(WIN32)
+        set(TinyXML2_LIB_NAME "tinyxml2.lib")
+    elseif(LINUX)
+        set(TinyXML2_LIB_NAME "libtinyxml2.a")
+    elseif(APPLE)
+        set(TinyXML2_LIB_NAME "libtinyxml2.a")
+    else()
+        message(FATAL_ERROR "Unsupported OS")
+    endif()
+
+    ExternalProject_Add(TinyXML2_external
+        GIT_REPOSITORY https://github.com/leethomason/tinyxml2.git
+        GIT_TAG 10.1.0
+        CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${TinyXML2_INSTALL_DIR}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DBUILD_SHARED_LIBS=${TynyXML2_BUILD_SHARED_LIBS}
+            -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+        BUILD_BYPRODUCTS "${TinyXML2_INSTALL_DIR}/lib/${TinyXML2_LIB_NAME}"
+    )
+
+    # Ensure directory exists at configure time
+    file(MAKE_DIRECTORY "${TinyXML2_INSTALL_DIR}/include")
+
+    # Define imported target
+    add_library(tinyxml2::tinyxml2 UNKNOWN IMPORTED)
+    set_target_properties(tinyxml2::tinyxml2 PROPERTIES
+        IMPORTED_LOCATION "${TinyXML2_INSTALL_DIR}/lib/${TinyXML2_LIB_NAME}"
+        INTERFACE_INCLUDE_DIRECTORIES "${TinyXML2_INSTALL_DIR}/include"
+    )
+
+    # Ensure all targets depending on TinyXML2 wait for it to build
+    add_dependencies(tinyxml2::tinyxml2 TinyXML2_external)
+endfunction()
